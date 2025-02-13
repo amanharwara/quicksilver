1de0f0e8d42129b33d81e7b6bb5b653a7eb1feaf
Aman Harwara <amanharwara@protonmail.com>
2025-02-14 01:36:59 +0530

    remove firefox support from dev build

--- build.sh
+++ build.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
 BROWSER="${1:-"chromium"}"
+PARENT_DIST_DIR="./.dist" 
+DIST="$PARENT_DIST_DIR/$BROWSER"
 
-export DIST="./.dist/${BROWSER}"
+# Minify CSS
+for file in ./src/*.css
+do
+	if [ -f $file ]; then
+		cat $file | tr -d '\n' | sed -E 's/\s?(\{|;|:)\s+/\1/g' > $DIST/"${file#./src/}"
+	fi
+done
 
-./minify-css.sh
-
+# Copy static code
 cp manifest.json $DIST/manifest.json
 cp ./src/popup.html $DIST/popup.html
 
-if [ $BROWSER="firefox" ]; then
+# Add firefox-specific manifest config
+if [ $BROWSER = "firefox" ]; then
 	cat manifest.json | jq --slurpfile f firefox.json '. + $f.[0]' > $DIST/manifest.json
 fi
 
+# Compile TS files
 tsc ./src/*.ts --outDir "$DIST"
 
+ZIP="$PARENT_DIST_DIR/$BROWSER.zip" 
+
+# Remove previous zip
+if [ -f $ZIP ]; then
+	rm $ZIP
+fi
+
+# Zip 
+zip -j $ZIP $DIST/* > /dev/null
+
+echo $ZIP
--- dev.sh
+++ dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BROWSER="${1:-"chromium"}"
+BROWSER="chromium"
 
 ./build.sh $BROWSER
 
@@ -12,11 +12,7 @@ PID_LIST+=" $pid"
 TEMP_PROFILE=`mktemp -d`
 echo "Created temp profile for browser, $TEMP_PROFILE"
 
-if [ $BROWSER = "firefox" ]; then
-	firefox-developer-edition -profile "$TEMP_PROFILE" -no-remote -new-instance http://localhost:5500 & pid=$!
-else
-	chromium --user-data-dir=$TEMP_PROFILE --load-extension="./.dist/$BROWSER" http://localhost:5500 & pid=$!
-fi
+chromium --user-data-dir=$TEMP_PROFILE --load-extension="./.dist/$BROWSER" http://localhost:5500 & pid=$!
 
 PID_LIST+=" $pid"
 
